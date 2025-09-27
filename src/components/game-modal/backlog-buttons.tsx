4cc6cc80a47ab49da704fcea9f92b0e3be727220
athavus <miguelryanfreitas@gmail.com>
2025-09-26 21:41:09 -0300

    adicionei o componente dos botões, ainda sem estarem em nenhum local de fato na interface, mas tudo bem.

--- src/components/game-modal/backlog-buttons.tsx
+++ src/components/game-modal/backlog-buttons.tsx
@@ -2,7 +2,7 @@
 import { BacklogButton } from "./backlog-button.tsx";
 //Bibliotecas
 import { ReactNode } from "react"
-import { eye, Shuffle, ListPlus } from "lucide-react"
+import { Eye, Shuffle, ListPlus } from "lucide-react"
 
 
 interface BacklogButtonsProps {
@@ -19,7 +19,11 @@ export function BacklogButtons(
   onAddToList
 ): BacklogButtonsProps {
   return (
-
+    <div className="flex justify-between gap-2 mt-4">
+      <BacklogButton icon={<Shuffle className="w-5 h-5" />} text={inRoleta ? "Remover da Roleta" : "Adicionar a Roleta"} onClick={onToggleRoleta}/>
+      <BacklogButton icon={<Eye className="w-5 h-5" />} text="Review" onClick={onReview}/>
+      <BacklogButton icon={<ListPlus className="w-5 h-5" />} text="Adicionar a Lista" onClick={onAddToList}/>
+    </div>
   );
 }
 
