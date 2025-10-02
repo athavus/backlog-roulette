d8e3631048a1e034ab6152ee710c0999fd34e8be
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2025-10-02 14:23:12 -0300

    fix: tentativa de ajeitar um pequeno problema no título em produção, que está com um bug no tamanho

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -2,11 +2,11 @@ export function BacklogTitle() {
   return (
     <div className="mb-4 sm:mb-6 lg:mb-8 w-full flex justify-center overflow-x-auto">
       <pre className="
-        text-[7px]                // Muito pequeno no mobile, cabe na largura
-        sm:text-[10px]            // Pouco maior em tablets pequenos
+        text-[5.5px]                // Muito pequeno no mobile, cabe na largura
+        sm:text-[9px]            // Pouco maior em tablets pequenos
         md:text-xs                // xs: 0.75rem em tablets/médios
-        lg:text-sm                // sm: 0.875rem em desktop
-        xl:text-base              // base: 1rem em telas grandes
+        lg:text-base                // sm: 0.875rem em desktop
+        xl:text-base              // base: 0rem em telas grandes
         text-blue-900
         font-mono
         leading-none
